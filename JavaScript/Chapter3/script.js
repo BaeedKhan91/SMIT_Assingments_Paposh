ed44fab7ef0c829cffc79836de2d44e831f055d9
Baeed khan <baeedk7@gmail.com>
2025-02-26 16:46:26 +0500

    Chapter3

---
+++ JavaScript/Chapter3/script.js
@@ -0,0 +1,25 @@
+
+// Question 1
+
+var age = 17;
+
+ alert("I am " + age + "Years old");
+
+// Question 2
+
+
+
+// Question 3
+
+var birthYear = 2007;
+
+ document.write("My birth year is " + birthYear + " <br> Data Type of my declared variable is number");
+
+// Question 4
+
+
+var visitorName = "Jhon";
+var productTitle = "T-Shirts";
+var quantity = 8;
+
+document.write(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing Store")
