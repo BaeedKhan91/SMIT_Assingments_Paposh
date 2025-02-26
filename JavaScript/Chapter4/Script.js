5b1010a04337e0db83b65b51fb9f4104912248db
Baeed khan <baeedk7@gmail.com>
2025-02-26 16:46:34 +0500

    Chapter4

---
+++ JavaScript/Chapter4/Script.js
@@ -0,0 +1,25 @@
+// Question 1
+
+var name = "Baeed", lastName = "khan", age = 17;
+
+// Question 2
+
+// legal
+
+// var userName;
+// var $userName;
+// var _userName;
+// var userName456;
+// var username;
+
+// // illegal
+
+// var 95userName;
+// var @userName;
+// var user name;
+// var user-name;
+// var var;
+
+
+
+
