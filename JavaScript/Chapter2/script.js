c57f2c2f08b49490013d95e92aa14984b075a38b
Baeed khan <baeedk7@gmail.com>
2025-02-26 16:46:11 +0500

    Chapter2

---
+++ JavaScript/Chapter2/script.js
@@ -0,0 +1,45 @@
+// Question 1
+var userName;
+
+// Question 2
+var myName = "Baeed Khan";
+
+// Question 3
+var titleMessage;
+titleMessage = "Hello World";
+alert(titleMessage);
+
+// Question 4
+var studentName = "jhoneDoe";
+var studentAge = 15;
+var studentBio = "Certified Mobile Application Development";
+
+alert(studentName);
+alert(studentAge + " years old");
+alert(studentBio);
+
+// Question 5
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+
+alert(pizza);
+
+// Question 6
+
+var email = "Baeedk7@gmail.com";
+alert("My email address is" + email);
+
+
+// Question 7
+
+var book = "A Smarter way to Learn JavaScript";
+alert("i am tryin to learn from the book" + book);
+
+// Question  8
+
+document.write("Yah! I can Write HTML content through JavaScript");
+
+// Question 9
+
+alert ("              ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+
