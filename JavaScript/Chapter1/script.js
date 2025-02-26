3d573858660c896072368786b7b8ba4404d744aa
Baeed khan <baeedk7@gmail.com>
2025-02-26 16:45:45 +0500

    Chapter1

--- JavaScript/Chapter1/script.js
+++ JavaScript/Chapter1/script.js
@@ -4,16 +4,17 @@ alert ("Hello User");
 
 // Question 2
 
-// alert ("Error! Please enter a valid password");
+alert ("Error! Please enter a valid password");
 
 // Question 3
 
-// alert("Welcome to Js Land.....\nHappy Coding")
+alert("Welcome to Js Land.....\nHappy Coding")
 
 // Question 4
 
+alert ("Welcome to js land");
+alert ("Happy Coding\n prevent this page from additional dialogs");
 
 
-// Question 5 
 
 
