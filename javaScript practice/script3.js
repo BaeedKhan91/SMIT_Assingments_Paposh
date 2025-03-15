848f819c8690f56789b0b74acc7b303ae92cb16c
Baeed khan <baeedk7@gmail.com>
2025-03-15 23:16:30 +0500

    javaScript practice

---
+++ javaScript practice/script3.js
@@ -0,0 +1,69 @@
+// let userName = prompt("What is your name").toUpperCase();
+
+
+// let userAge = +prompt("What is your Age");
+
+// if (userAge >= 18 ) {
+//     alert("YOur Are eligble for voting");
+// }else{
+//     alert("your are not eligible for voting");
+// }
+
+// let userData ={
+//     name : userName,
+//     Age : userAge,
+// }
+
+// console.log(userData);
+
+
+// let products =[
+//     {
+//         name : "airpods",
+//         price : 10000,
+//         quantity : 8
+//     },
+//     {
+//         name : "laptop",
+//         price : 80000,
+//         quantity : 5
+//     },
+//     {
+//         name : "mobile",
+//         price : 50000,
+//         quantity : 16
+//     },
+// ]
+
+
+// let userProduct = prompt("which product you have to search");
+// let found = false;
+// userProduct = userProduct.toLowerCase();
+
+// debugger
+
+// for (let i = 0; i < products.length; i++ ){
+//     if (products[i].name === userProduct) {
+//         console.log(products[i].price);
+//         console.log(products[i].quantity);
+//         found = true;
+//         break;
+//     }
+//     }
+
+//     if (!found) {
+//         alert("prodcut not found ")
+//     }
+    
+
+let firstD = +prompt("Age kia hai tumhari");
+
+let brothers = +prompt("kitne bhai hai tere ");
+
+if (brothers === 0) {
+       
+}
+// let secondD = +prompt("Second number");
+
+
+// console.log(firstD+secondD);
