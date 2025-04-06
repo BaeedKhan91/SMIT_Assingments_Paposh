f72875606424c9309736866232a9d732f1c10176
Baeed khan <baeedk7@gmail.com>
2025-04-06 17:00:39 +0500

    JavaScript Assingment 04

---
+++ JavaScript Assingment/JavaScript Assingment 04/Script.js
@@ -0,0 +1,35 @@
+
+// // Exercise 1: Simple Addition
+// // Write a function that takes two numbers and returns their sum.
+
+// function sum (a = 0,b = 0){
+//    let ans = a + b;
+//    return ans
+// }
+
+// let result = sum();
+
+// console.log(result);
+
+
+
+// // Exercise 2: Check Even or Odd
+// // Create a function that takes a number and returns "even" if it's even, and "odd" if it's odd.
+
+// function checkEvenOdd (num = 0){
+//     let ans = "even";
+//     if (num % 2 === 0) {
+//         ans = "even";
+//     }
+//     else {
+//         ans = "odd";
+//     }
+//     return ans
+// }
+
+// let result = checkEvenOdd(5);
+
+// console.log(result);
+
+
+
