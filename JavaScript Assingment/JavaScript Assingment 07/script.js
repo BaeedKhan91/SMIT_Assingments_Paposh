e205930d8c4645c01af7074c61b465b3fb3b1f6d
Baeed khan <baeedk7@gmail.com>
2025-06-04 16:24:26 +0500

    adding edit functionality

--- JavaScript Assingment/JavaScript Assingment 07/script.js
+++ JavaScript Assingment/JavaScript Assingment 07/script.js
@@ -1,32 +1,101 @@
 const addBtn = document.querySelector("#add-task");
 const ul = document.querySelector(".ul");
+const submitForm = document.querySelector("#submit-form")
 
-addBtn.addEventListener("click", () => {
+let editindex = null
+submitForm.addEventListener("submit", () => {
+
     const input = document.querySelector("#input");
-    if (input.value === "") {
-        alert("Please Write Some Task");
+    const items = ul.children
+    if (editindex !== null) {
+        const li = items[editindex]
+        li.textContent = input.value
+        const spanC = document.createElement("span");
+        spanC.classList.add("checkbox");
+        const spanD = document.createElement("span");
+        spanD.classList.add("delete");
+        const spanE = document.createElement("span")
+        spanE.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`
+        spanE.classList.add("edit");
+        
+
+        const spanM = document.createElement("span")
+        spanC.innerHTML = `<i class="fa-solid check fa-check"></i>`;
+        spanD.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+        li.innerHTML = input.value;
+        spanM.appendChild(spanE)
+        spanM.appendChild(spanC);
+        spanM.appendChild(spanD);
+        li.appendChild(spanM)
+        addBtn.innerHTML = "ADD Todo"
+        editindex = null
+        input.value = ""
+        saveData()
+    }else{
+
+        const input = document.querySelector("#input");
+        if (input.value === "") {
+            alert("Please Write Some Task");
     } else {
         const li = document.createElement("li");
         const spanC = document.createElement("span");
         spanC.classList.add("checkbox");
         const spanD = document.createElement("span");
         spanD.classList.add("delete");
-        spanC.innerHTML = `<i class="fa-solid fa-check"></i>`;
+        const spanE = document.createElement("span")
+        spanE.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`
+        spanE.classList.add("edit");
+        
+
+        const spanM = document.createElement("span")
+        spanC.innerHTML = `<i class="fa-solid check fa-check"></i>`;
         spanD.innerHTML = `<i class="fa-solid fa-trash"></i>`;
         li.innerHTML = input.value;
-        li.appendChild(spanC);
-        li.appendChild(spanD);
+        spanM.appendChild(spanE)
+        spanM.appendChild(spanC);
+        spanM.appendChild(spanD);
+        li.appendChild(spanM)
         ul.appendChild(li);
         input.value = "";
+        saveData()
     }
+}
 });
 
 ul.addEventListener("click", (e) => {
     if (e.target.parentElement.classList.contains("delete")) {
-        e.target.parentElement.parentElement.remove();
+        e.target.parentElement.parentElement.parentElement.remove();
+        saveData()
     }
-
+    
     else if (e.target.parentElement.classList.contains("checkbox")) {
-        e.target.parentElement.parentElement.classList.toggle("checked");
+        e.target.parentElement.parentElement.parentElement.classList.toggle("checked");
+        saveData()
+    }
+    else if (e.target.parentElement.classList.contains("edit")) {
+        const text = e.target.parentElement.parentElement.parentElement.textContent;
+        const input = document.querySelector("#input")
+        input.value = text
+        addBtn.innerHTML = " Edit Todo"
+        const item = ul.children
+        for(let i =0 ;i < item.length; i++){
+            if (item[i].textContent === text) {
+                editindex = i
+                
+                
+                
+            }
+            
+        }
+        saveData()
     }
 });
+
+
+function saveData () {
+    localStorage.setItem("data" , ul.innerHTML)
+}
+function getData (){
+    ul.innerHTML = localStorage.getItem("data")
+}
+getData()
