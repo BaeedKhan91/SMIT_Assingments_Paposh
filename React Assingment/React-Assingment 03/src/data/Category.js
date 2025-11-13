527ab623ad7824d0038b1d6a10bc4a0ff6bafb69
BaeedKhan91 <baeedk7@gmail.com>
2025-11-14 03:26:01 +0500

    React Collection Page

---
+++ React Assingment/React-Assingment 03/src/data/Category.js
@@ -0,0 +1,19 @@
+export const categories = [
+  {
+    id: 1,
+    title: "Men",
+    image: "",
+  },
+  {
+    id: 2,
+    title: "Women",
+    image: "",
+  },
+  {
+    id: 3,
+    title: "Kids",
+    image: "",
+  },
+];
+
+export const categoryTitle = categories.map((category) => category.title);
---
+++ React Assingment/React-Assingment 03/src/data/products.js
@@ -0,0 +1,122 @@
+export const products = [
+  {
+    id: 9,
+    image: "/images/Products/p_img18.png",
+    title: "boy Round Neck Pure Cotton T-shirt",
+    description:
+      "A lightweight pullover shirt with a round neckline and short sleeves, worn as an outerwear.",
+    price: 900,
+    category: "Kids",
+    rating: 3,
+  },
+  {
+    id: 4,
+    image: "/images/Products/p_img10.png",
+    title: " Men Tapered Fit Flat-Front Trousers",
+    description:
+      "A lightweight pullover shirt with a round neckline and short sleeves, worn as an outerwear.",
+    price: 400,
+    category: "Men",
+    rating: 5,
+  },
+  {
+    id: 11,
+    image: "/images/Products/p_img22.png",
+    title: "Women Palazzo Pants with Waist Belt",
+    description:
+      "A lightweight pullover shirt with a round neckline and short sleeves, worn as an outerwear.",
+    price: 1100,
+    category: "Women",
+    rating: 2,
+  },
+  {
+    id: 1,
+    image: "/images/Products/p_img11.png",
+    title: "Men Round Neck Pure Cotton T-shirt",
+    description:
+      "A lightweight pullover shirt with a round neckline and short sleeves, worn as an outerwear.",
+    price: 100,
+    category: "Men",
+    rating: 4,
+  },
+  {
+    id: 6,
+    image: "/images/Products/p_img16.png",
+    title: "Girls Round Neck Cotton Top",
+    description:
+      "A lightweight pullover shirt with a round neckline and short sleeves, worn as an outerwear.",
+    price: 600,
+    category: "Kids",
+    rating: 4,
+  },
+  {
+    id: 5,
+    image: "/images/Products/p_img20.png",
+    title: "Women Palazzo Pants with Waist Belt",
+    description:
+      "A lightweight pullover shirt with a round neckline and short sleeves, worn as an outerwear.",
+    price: 500,
+    category: "Women",
+    rating: 2,
+  },
+  {
+    id: 3,
+    image: "/images/Products/p_img14.png",
+    title: "boy Round Neck Pure Cotton T-shirt",
+    description:
+      "A lightweight pullover shirt with a round neckline and short sleeves, worn as an outerwear.",
+    price: 300,
+    category: "Kids",
+    rating: 2,
+  },
+  {
+    id: 12,
+    image: "/images/Products/p_img23.png",
+    title: "boy Round Neck Pure Cotton T-shirt",
+    description:
+      "A lightweight pullover shirt with a round neckline and short sleeves, worn as an outerwear.",
+    price: 1200,
+    category: "Kids",
+    rating: 1,
+  },
+  {
+    id: 10,
+    image: "/images/Products/p_img15.png",
+    title: "Men Tapered Fit Flat-Front Trousers",
+    description:
+      "A lightweight pullover shirt with a round neckline and short sleeves, worn as an outerwear.",
+    price: 1000,
+    category: "Men",
+    rating: 1,
+  },
+  {
+    id: 2,
+    image: "/images/Products/p_img13.png",
+    title: "Women Round Neck Cotton Top",
+    description:
+      "A lightweight pullover shirt with a round neckline and short sleeves, worn as an outerwear.",
+    price: 200,
+    category: "Women",
+    rating: 5,
+  },
+  {
+    id: 8,
+    image: "/images/Products/p_img21.png",
+    title: "Women Zip-Front Relaxed Fit Jacket",
+    description:
+      "A lightweight pullover shirt with a round neckline and short sleeves, worn as an outerwear.",
+    price: 800,
+    category: "Women",
+    rating: 3,
+  },
+  {
+    id: 7,
+    image: "/images/Products/p_img12.png",
+    title: "Men Round Neck Pure Cotton T-shirt",
+    description:
+      "A lightweight pullover shirt with a round neckline and short sleeves, worn as an outerwear.",
+    price: 700,
+    category: "Men",
+    rating: 3,
+  },
+];
